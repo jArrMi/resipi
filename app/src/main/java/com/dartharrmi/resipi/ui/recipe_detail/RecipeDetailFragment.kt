88e44175154e0bbaaf40088a2812978a9908c3ce
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2020-08-03 22:25:44 -0500

    [RSP-005]: Add RecipeDetailFragment

--- app/src/main/java/com/dartharrmi/resipi/ui/recipe_detail/RecipeDetailFragment.kt
+++ app/src/main/java/com/dartharrmi/resipi/ui/recipe_detail/RecipeDetailFragment.kt
@@ -1,4 +1,16 @@
 package com.dartharrmi.resipi.ui.recipe_detail
 
-class RecipeDetailFragment {
+import com.dartharrmi.resipi.R
+import com.dartharrmi.resipi.base.ResipiFragment
+import com.dartharrmi.resipi.databinding.FragmentRecipeDetailBinding
+
+class RecipeDetailFragment : ResipiFragment<FragmentRecipeDetailBinding>() {
+
+    override fun getLayoutId() = R.layout.fragment_recipe_detail
+
+    override fun getVariablesToBind(): Map<Int, Any> = emptyMap()
+
+    override fun initObservers() {
+
+    }
 }
