db032e635d70bbbfa5b9237476df95e3e05ac95b
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2020-08-04 22:37:00 -0500

    [RSP-005]: Create custom view for showing recipe insights on the details screen

---
+++ app/src/main/java/com/dartharrmi/resipi/ui/views/RecipePillInfoView.kt.kt
@@ -0,0 +1,41 @@
+package com.dartharrmi.resipi.ui.views
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.dartharrmi.resipi.R
+
+class RecipePillInfoView: RelativeLayout {
+
+    private var drawable: Drawable? = null
+    private var text: String? = null
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        LayoutInflater.from(context).inflate(R.layout.view_recipe_pill, this);
+
+
+        attrs?.let {
+            with(attrs) {
+                context.obtainStyledAttributes(it, R.styleable.RecipePillInfoView).also {
+                    findViewById<ImageView>(R.id.recipePillIcon).setImageDrawable(it.getDrawable(R.styleable.RecipePillInfoView_pillIcon))
+                    findViewById<TextView>(R.id.recipePillText).text = it.getString(R.styleable.RecipePillInfoView_pillText)
+                    it.recycle()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dartharrmi/resipi/utils/Utils.kt
@@ -0,0 +1,2 @@
+package com.dartharrmi.resipi.utils
+
